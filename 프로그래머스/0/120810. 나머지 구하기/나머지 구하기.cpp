f7e6780ce7110332bda59f1a94f2e6233900b58d
Serin Cheong <serin.cheong@gmail.com>
2024-09-21 18:39:59 +0900

    [level 0] Title: 나머지 구하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer;
+    
+    answer = num1 % num2;
+    
+    return answer;
+}
