9d2f8e32af8df2e3608395414d190f94aa043cc1
Serin Cheong <serin.cheong@gmail.com>
2024-09-21 18:43:55 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int age) {
+    int answer;
+    
+    answer = 2022 - age + 1;
+    
+    return answer;
+}
